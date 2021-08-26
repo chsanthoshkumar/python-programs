fe9a08cc77bd15fd0b306be485326ac9fb451456
chsanthoshkumar <79572118+chsanthoshkumar@users.noreply.github.com>
2021-08-26 09:10:18 +0530

    Add files via upload

---
+++ primee.py
@@ -0,0 +1,13 @@
+lower=int(input("enter lower"))
+upper=int(input("enter upper"))
+
+print("Prime numbers between", lower, "and", upper, "are:")
+
+for num in range(lower, upper + 1):
+   
+   if num > 1:
+       for i in range(2, num):
+           if (num % i) == 0:
+               break
+       else:
+           print(num)
