3123ed5a9b3afc205eb70d930d44b3eb2edcff51
chsanthoshkumar <79572118+chsanthoshkumar@users.noreply.github.com>
2021-09-22 14:51:32 +0530

    Update 13_Joins_tuples.py

--- 13_Joins_tuples.py
+++ 13_Joins_tuples.py
@@ -1,10 +1,9 @@
 test_list = [(5, 6), (5, 7), (6, 8), (6, 10), (7, 13)]
   
-# printing original list
 print("The original list is : " + str(test_list))
   
-# Join Tuples if similar initial element
-# Using loop
+
+
 res = []
 for sub in test_list:                                           
     if res and res[-1][0] == sub[0]:              
@@ -13,5 +12,5 @@ for sub in test_list:
         res.append([ele for ele in sub])     
 res = list(map(tuple, res))
   
-# printing result 
+ 
 print("The extracted elements : " + str(res)) 
