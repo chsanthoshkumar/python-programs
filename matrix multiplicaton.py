e385d65045d73827454e8a89f4349f7bbdcd87b4
chsanthoshkumar <79572118+chsanthoshkumar@users.noreply.github.com>
2021-09-22 15:22:14 +0530

    Update matrix multiplicaton.py

--- matrix multiplicaton.py
+++ matrix multiplicaton.py
@@ -11,9 +11,9 @@ result = [[0,0,0,0],
          [0,0,0,0]]
 
 for i in range(len(X)):
-   # iterate through columns of Y
+ 
    for j in range(len(Y[0])):
-       # iterate through rows of Y
+   
        for k in range(len(Y)):
            result[i][j] += X[i][k] * Y[k][j]
 
